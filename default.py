30c396a81b56238bcd8cfcbc694485cecaac7ca0
Bubbles <testmailqv@protonmail.ch>
2022-09-27 14:14:08 +0100

    User options and add episodes
    Added user configuration options to enable/disable subtitles and sign language support.
    Added streaming of the current and previous episodes to the plugin.

--- default.py
+++ default.py
@@ -1,9 +1,20 @@
 # -*- coding: utf-8 -*-
+"""
+Attributes:
+        subtitle (bool): Display subtitles with the video
+        signlang (bool): Use the sign-language version of the video
+        viddesc  (bool): Use the video description version of the video
+"""
 import libwdr
 import re
 import requests
+import xbmcaddon
 
-xml = 'http://www.wdrmaus.de/_export/videositemap.php5'
+ArchiveXml = 'http://www.wdrmaus.de/_export/videositemap.php5'
+CurrentXml = 'http://www.wdrmaus.de/aktuelle-sendung/index.php5'
+PreviousXml = 'http://www.wdrmaus.de/aktuelle-sendung/vorwoche.php5'
+
+ADDON = xbmcaddon.Addon(id='plugin.video.wdrmaus')
 
 class wdrmaus(libwdr.libwdr):
 	def __init__(self):
@@ -11,9 +22,28 @@ class wdrmaus(libwdr.libwdr):
 		self.modes['mausListVideos'] = self.mausListVideos
 
 	def libWdrListMain(self):
-		response = requests.get(xml).text
-		categories = re.compile('<video:category>(.+?)</video:category>', re.DOTALL).findall(response)
 		result = {'items':[],'name':'root'}
+
+		response = requests.get(CurrentXml).text
+		d = {'metadata':{'art':{}}, 'params':{'mode':'libWdrPlayJs'}, 'type':'video'}
+		d['metadata']['name'] = (re.compile('name\" : \"(.+?)\"', re.DOTALL).findall(response))[1]
+		d['metadata']['plot'] = (re.compile('<p>(.+?)\</p>', re.DOTALL).findall(response))[0]
+		d['metadata']['art']['thumb'] = (re.compile('thumbnailURL\" : \[ \"(.+?)\"', re.DOTALL).findall(response))[0]
+		d['params']['url'] = re.search('https://kinder(.+?)jsonp', response)[0]
+		d['params']['uservideomode'] = self.getVideoMode()
+		result['items'].append(d)
+		
+		response = requests.get(PreviousXml).text
+		d = {'metadata':{'art':{}}, 'params':{'mode':'libWdrPlayJs'}, 'type':'video'}
+		d['metadata']['name'] = (re.compile('name\" : \"(.+?)\"', re.DOTALL).findall(response))[1]
+		d['metadata']['plot'] = (re.compile('<p>(.+?)\</p>', re.DOTALL).findall(response))[0]
+		d['metadata']['art']['thumb'] = (re.compile('thumbnailURL\" : \[ \"(.+?)\"', re.DOTALL).findall(response))[0]
+		d['params']['url'] = re.search('https://kinder(.+?)jsonp', response)[0]
+		d['params']['uservideomode'] = self.getVideoMode()
+		result['items'].append(d)
+		
+		response = requests.get(ArchiveXml).text
+		categories = re.compile('<video:category>(.+?)</video:category>', re.DOTALL).findall(response)
 		names = []
 		for cat in categories:
 			if not cat in names:
@@ -23,7 +53,7 @@ class wdrmaus(libwdr.libwdr):
 	
 	def mausListVideos(self):
 		result = {'items':[]}
-		response = requests.get(xml).text
+		response = requests.get(ArchiveXml).text
 		videos = re.compile('<url>(.+?)</url>', re.DOTALL).findall(response)
 		for video in videos:
 			if self.params['cat'] == re.compile('<video:category>(.+?)</video:category>', re.DOTALL).findall(video)[0]:
@@ -31,7 +61,18 @@ class wdrmaus(libwdr.libwdr):
 				d['metadata']['name'] = re.compile('<video:title><!\[CDATA\[(.+?)\]\]></video:title>', re.DOTALL).findall(video)[0].replace('![CDATA[','').replace(']]','')
 				d['metadata']['art']['thumb'] = re.compile('<video:thumbnail_loc>(.+?)</video:thumbnail_loc>', re.DOTALL).findall(video)[0].replace('<![CDATA[','').replace(']]>','')
 				d['params']['url'] = re.compile('<video:player_loc.+?>(.+?)</video:player_loc>', re.DOTALL).findall(video)[0].replace('<![CDATA[','').replace(']]>','')
+				d['params']['uservideomode'] = self.getVideoMode()
 				result['items'].append(d)
+		return result
+	
+	def getVideoMode(self):
+		result = 'none'
+		if ADDON.getSetting('subtitle') == 'true':
+			result = 'subtitle'
+		if ADDON.getSetting('signlang') == 'true':
+			result = 'signlang'
+		if ADDON.getSetting('viddesc') == 'true':
+			result = 'viddesc'
 		return result
 
 o = wdrmaus()
